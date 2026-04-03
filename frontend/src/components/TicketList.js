e36fb9b25b15a1874a3f5031480ec24673d7f201
a786z000001 <213057175+a786z000001@users.noreply.github.com>
2026-04-03 14:00:03 +0530

    Update TicketList.js

--- frontend/src/components/TicketList.js
+++ frontend/src/components/TicketList.js
@@ -1,38 +1,24 @@
 import { useEffect, useState } from "react";
 
-function TicketList({ refreshKey }) {
+function TicketList({refresh}) {
   const [tickets, setTickets] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/tickets")
+    fetch("http://localhost:8001/tickets")
       .then((res) => res.json())
-      .then((data) => setTickets(data))
-      .catch(() => setTickets([]));
-  }, [refreshKey]);
+      .then((data) => setTickets(data));
+  }, [refresh]);
 
   return (
-    <div>
-      <h2>Previously Analyzed Tickets</h2>
-      <table width="100%" border="1" cellPadding="8" style={{ borderCollapse: "collapse" }}>
-        <thead>
-          <tr>
-            <th>Message</th>
-            <th>Category</th>
-            <th>Priority</th>
-            <th>Confidence</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tickets.map((t) => (
-            <tr key={t.id}>
-              <td>{t.message}</td>
-              <td>{t.category}</td>
-              <td>{t.priority}</td>
-              <td>{t.confidence}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div style={{ marginTop: 30 }}>
+      <h2>Previous Tickets</h2>
+
+      {tickets.map((t) => (
+        <div key={t.id} style={{ border: "1px solid #ccc", margin: 10, padding: 10 }}>
+          <p><b>{t.message}</b></p>
+          <p>{t.category} | {t.priority}</p>
+        </div>
+      ))}
     </div>
   );
 }
